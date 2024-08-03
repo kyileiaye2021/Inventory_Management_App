afb57bd1bee1643ff57e9c60bd1d7c24e04c51bc
aye, kyi lei <98507949+kyileiaye2021@users.noreply.github.com>
2024-08-03 15:28:54 -0700

    made config private

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDCk30B0Tok91zfRdl0pl5Exmy5UnBCH8I",
-  authDomain: "inventory-management-app-2868e.firebaseapp.com",
-  projectId: "inventory-management-app-2868e",
-  storageBucket: "inventory-management-app-2868e.appspot.com",
-  messagingSenderId: "711768367930",
-  appId: "1:711768367930:web:24e7723c275b945ea1a603",
-  measurementId: "G-PP5N9DMJV7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
